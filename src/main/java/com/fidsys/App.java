a8a4ce737029f8d3b1da7773ae2634f8bfed699f
cristobalP02 <cristobal.perezb@usm.cl>
2025-07-08 22:19:47 -0400

    creacion menu

--- src/main/java/com/fidsys/App.java
+++ src/main/java/com/fidsys/App.java
@@ -1,7 +1,21 @@
 package com.fidsys;
 
+import java.io.Console;
+import java.util.ArrayList;
+
 public class App{
+
+    private static ArrayList<cliente> lista_clientes = new ArrayList<cliente>();
+    private static ArrayList<compra> lista_compras = new ArrayList<compra>();
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Boolean menu = false;
+
+        Console console = System.console();
+
+        while(menu){
+
+        }
+
+        System.out.println("Cerrando la aplicación");
     }
 }
